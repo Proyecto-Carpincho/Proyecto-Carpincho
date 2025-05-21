a9277ce82d362778c07cc94ef9c81fcc68219790
Luis-M-S <142127420+Luis-M-S@users.noreply.github.com>
2025-05-21 16:06:27 -0300

    segunda version enemigo
    puede moverse por las paredes puede caer hacia el piso, tiene gravedad pero hay bug visuales y el como cae al piso se puede mejorar con un ray cast

--- Scripts/enemigo.gd
+++ Scripts/enemigo.gd
@@ -2,12 +2,28 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const GravedadMax = 300
 @export var Personaje:CharacterBody2D
 @onready var Navegacion:NavigationAgent2D = $NavigationAgent2D
 var PosicionAir:Vector2
 
 func _physics_process(delta: float) -> void:
-	velocity = global_position.direction_to(Navegacion.get_next_path_position()) * SPEED
+	if global_position.direction_to(Navegacion.get_next_path_position()).length() < 5:
+		global_position.x=move_toward(global_position.x, Navegacion.get_next_path_position().x,delta*SPEED)
+		global_position.y=move_toward(global_position.y, Navegacion.get_next_path_position().y,delta*SPEED)
+	else:
+		velocity.x = global_position.direction_to(Navegacion.get_next_path_position()).x * SPEED
+		velocity.y += global_position.direction_to(Navegacion.get_next_path_position()).y * SPEED
+	
+	var auxGravedad = get_gravity() if not is_on_wall_only() else Vector2.ZERO
+	if not is_on_floor():
+		if velocity.y + auxGravedad.y < GravedadMax:
+			velocity.y += auxGravedad.y * delta
+		else:
+			velocity.y = GravedadMax
+	
+	
+	
 	move_and_slide()
 
 func _AreaDeteccion_bodyEntered(body: Node2D) -> void:
